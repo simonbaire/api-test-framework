f4a45169b3e7ee2e097f5b5c085c03833f32090f
simonbaire <simon.baire@hotmail.co.uk>
2025-03-05 20:11:33 +0000

    add test

--- cypress/e2e/api.spec.cy.ts
+++ cypress/e2e/api.spec.cy.ts
@@ -1,9 +1,16 @@
 // api-test-framework/cypress/integration/api.spec.ts
 describe('API Tests', () => {
+
   it('should return a list of users', () => {
     cy.request('GET', '/users').then((response) => {
       expect(response.status).to.eq(200);
       expect(response.body).to.be.an('array');
+    });
+  });
+
+  it('should return a user with correct name', () => {
+    cy.request('GET', '/users').then((response) => {
+      expect(response.body[0].name).to.eq("Wrong name");
     });
   });
 });
